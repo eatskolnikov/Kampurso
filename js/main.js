28068b16e72644bf9f348216a487103392d4140d
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-10-14 19:50:15 -0500

    Base game works

---
+++ js/main.js
@@ -0,0 +1,76 @@
+console.log( "main.js" );
+
+main = {
+    settings : null,
+    canvasWindow : null,
+    states : {},
+    currentState : null,
+    
+    init : function( pSettings, pCanvasWindow ) {
+        // Initialize settings
+        main.settings = pSettings;
+        main.settings.width = $( "canvas" ).width();
+        main.settings.height = $( "canvas" ).height();
+        console.log( "Canvas window size: " + main.settings.width + "x" + main.settings.height );
+        main.canvasWindow = pCanvasWindow;
+
+        // Initialize event listeners
+        window.addEventListener( "mousedown",   main.click, false );
+        window.addEventListener( "keydown",     main.keydown, false );
+        window.addEventListener( "keyup",       main.keyup, false );
+
+        // Initialize states
+        main.initStates();
+        main.changeState( "titleState" );
+        
+        main.image = new Image();
+        main.image.src = "catship.png";
+    },
+
+    initStates : function() {
+        main.states.titleState = titleState;
+    },
+
+    changeState : function( name ) {
+        console.log( "Switch to state \"" + name + "\"..." );
+        if ( main.currentState != null )
+        {
+            main.currentState.Clear();
+        }
+        UI_TOOLS.ClearUI();
+        main.currentState = main.states[ name ];
+        main.currentState.Init( main.canvasWindow, main.settings );
+    },
+    
+    update : function() {
+    },
+    
+    draw : function() {
+        // Fill background
+        main.canvasWindow.fillStyle = "#333333";
+        main.canvasWindow.fillRect( 0, 0, main.settings.width, main.settings.height );
+        
+        // Draw current state
+        if ( main.currentState != null )
+        {
+            main.currentState.Draw();
+        }
+    },
+    
+    // Events
+    click : function( event )
+    {
+        console.log( event );
+    },
+    
+    keydown : function( event )
+    {
+        console.log( event );
+    },
+    
+    keyup : function( event )
+    {
+        console.log( event );
+    }
+};
+
---
+++ js/title.js
@@ -0,0 +1,89 @@
+console.log( "title.js" );
+
+titleState = {
+    canvas: null,
+    options: {},
+    images: {},
+    isDone: false,
+
+    Init: function( canvas, options ) {
+        titleState.canvas = canvas;
+        titleState.options = options;
+        titleState.isDone = false;
+
+        ASSET_TOOLS.AddSound( { title: "title",
+                            file: "assets/audio/DancingBunnies_Moosader.ogg",
+                            volume: 0.1,
+                            loop: true,
+                            isMusic: true
+                            } );
+
+        UI_TOOLS.CreateImage( { title: "background",
+                            src:    "assets/images/background.png",
+                            x: 0, y: 0,
+                            width: 640, height: 480,
+                            fullWidth: 640, fullHeight: 480 } );
+
+        UI_TOOLS.CreateImage( { title: "title",
+                            src:    "assets/images/title.png",
+                            x: ( titleState.options.width / 2 ) - ( 383 / 2 ),
+                            y: 25,
+                            width: 336, height: 158,
+                            fullWidth: 336, fullHeight: 158 } );
+
+
+        UI_TOOLS.CreateText( { title: "Version", words: "v1.0", color: "#000000", font: "bold 16px Courier", x: 450, y: 175 } );
+
+        var text = "by Rachel J. Morris (Moosader.com)";
+        UI_TOOLS.CreateText( { title: "by", words: text,
+                             color: "#000000", font: "bold 16px Courier",
+                             x: ( titleState.options.width / 2 ) - ( text.length * 9 ) / 2,
+                             y: options.height - 10 } );
+
+        UI_TOOLS.CreateButton( { title: "playButton", words: "Play!",
+                             color: "#ffffff", font: "bold 30px Courier",
+                             src: "assets/images/button.png",
+                             x: 100, y: options.height - 150,
+                             textX: 20, textY: 45,
+                             width: 125, height: 75,
+                             fullWidth: 125, fullHeight: 75,
+                             Click: function() {
+                                 main.ChangeState( "gameState" );
+                                 } } );
+
+        UI_TOOLS.CreateButton( { title: "helpButton", words: "Help!",
+                             color: "#ffffff", font: "bold 30px Courier",
+                             src: "assets/images/button.png",
+                             x: 400, y: options.height - 150,
+                             textX: 20, textY: 45,
+                             width: 125, height: 75,
+                             fullWidth: 125, fullHeight: 75,
+                             Click: function() {
+                                 main.ChangeState( "helpState" );
+                                 } } );
+
+        //SONO_ILO.LudiMuzikon( "menuoMuziko" );
+        ASSET_TOOLS.PlayMusic( "title" );
+    },
+
+    Clear: function() {
+        UI_TOOLS.ClearUI();
+    },
+
+    Click: function( ev ) {
+        UI_TOOLS.Click( ev );
+    },
+
+    KeyPress: function( ev ) {
+    },
+
+    KeyRelease: function( ev ) {
+    },
+
+    Update: function() {
+    },
+
+    Draw: function() {
+        UI_TOOLS.Draw( titleState.canvas );
+    },
+};
