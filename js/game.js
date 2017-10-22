ef18bfaf658427dab4f22909c2ae69a3d2306a20
Rachel "Wil Sha" Morris <racheljmorris@gmail.com>
2017-10-21 22:47:01 -0500

    Added camp ranger who cleans things up at night, and goes back to cabin during the day.

--- js/game.js
+++ js/game.js
@@ -13,30 +13,30 @@ bear = {
 
     Move: function( dir ) {
         if ( dir == "UP" ) {
-            bear.y -= bear.speed;
+            this.y -= this.speed;
         }
         else if ( dir == "DOWN" ) {
-            bear.y += bear.speed;
+            this.y += this.speed;
         }
         else if ( dir == "LEFT" ) {
-            bear.x -= bear.speed;
+            this.x -= this.speed;
         }
         else if ( dir == "RIGHT" ) {
-            bear.x += bear.speed;
+            this.x += this.speed;
         }
 
-        if ( bear.x < 0 ) {
-            bear.x = 0;
+        if ( this.x < 0 ) {
+            this.x = 0;
         }
-        else if ( bear.x + bear.width > 640 ) {
-            bear.x = 640 - bear.width;
+        else if ( this.x + this.width > 640 ) {
+            this.x = 640 - this.width;
         }
 
-        if ( bear.y < gameState.minObjectY ) {
-            bear.y = gameState.minObjectY;
+        if ( this.y < gameState.minObjectY ) {
+            this.y = gameState.minObjectY;
         }
-        else if ( bear.y + bear.height > 480 ) {
-            bear.y = 480 - bear.height;
+        else if ( this.y + this.height > 480 ) {
+            this.y = 480 - this.height;
         }
     },
 
@@ -46,15 +46,125 @@ bear = {
 
     Draw: function( canvas ) {
         canvas.drawImage(
-            bear.image,
-            0, 0, bear.width, bear.height,
-            bear.x, bear.y,
-            bear.fullWidth, bear.fullHeight );
+            this.image,
+            0, 0, this.width, this.height,
+            this.x, this.y,
+            this.fullWidth, this.fullHeight );
     },
 
     Eat: function( value ) {
         this.hunger -= value;
         if ( this.hunger < 0 ) { this.hunger = 0; }
+    }
+};
+
+nightRanger = {
+    x: 0,
+    y: 0,
+    width: 48,
+    height: 48,
+    fullWidth: 48,
+    fullHeight: 48,
+    speed: 2,
+    image: null,
+    isActive: true,
+    goalX: -1,
+    goalY: -1,
+    cabinX: 0,
+    cabinY: 100,
+    
+    Move: function( dir ) {
+        if ( dir == "UP" ) {
+            this.y -= this.speed;
+        }
+        else if ( dir == "DOWN" ) {
+            this.y += this.speed;
+        }
+        else if ( dir == "LEFT" ) {
+            this.x -= this.speed;
+        }
+        else if ( dir == "RIGHT" ) {
+            this.x += this.speed;
+        }
+
+        if ( this.x < 0 ) {
+            this.x = 0;
+        }
+        else if ( this.x + this.width > 640 ) {
+            this.x = 640 - this.width;
+        }
+
+        if ( this.y < gameState.minObjectY ) {
+            this.y = gameState.minObjectY;
+        }
+        else if ( this.y + this.height > 480 ) {
+            this.y = 480 - this.height;
+        }
+    },
+
+    Update: function() {
+        if ( gameState.isNight ) {
+            this.isActive = true;
+        }
+        else {
+            this.goalX = this.cabinX;
+            this.goalY = this.cabinY;
+        }
+
+        if ( this.goalX == -1 ) {
+            for ( var i = 0; i < gameState.items.length; i++ ) {
+                if ( gameState.items[i].type == "food" ) {
+                    this.goalX = gameState.items[i].x;
+                    this.goalY = gameState.items[i].y;
+                    console.log( "Ranger goal: ", this.goalX, this.goalY );
+                    break;
+                }
+            }
+        }
+
+        if ( this.x - 1 > this.goalX ) {
+            this.Move( "LEFT" );
+        }
+        else if ( this.x + 1 < this.goalX ) {
+            this.Move( "RIGHT" );
+        }
+        else if ( this.y - 1 > this.goalY ) {
+            this.Move( "UP" );
+        }
+        else {
+            this.Move( "DOWN" );
+        }
+
+        if ( gameState.isNight == false && this.x < this.cabinX + 64 && this.y < this.cabinY + 64 ) {
+            this.isActive = false;
+            this.ResetGoal();
+        }
+    },
+
+    Draw: function( canvas ) {
+        if ( this.isActive ) {
+            canvas.drawImage(
+                this.image,
+                0, 0, this.width, this.height,
+                this.x, this.y,
+                this.fullWidth, this.fullHeight );
+
+            canvas.drawImage(
+                gameState.images.cabins,
+                64, 0, 64, 64, this.cabinX, this.cabinY, 64, 64 
+                );
+        }
+        else {
+            canvas.drawImage(
+                gameState.images.cabins,
+                0, 0, 64, 64, this.cabinX, this.cabinY, 64, 64 
+                );
+        }
+    },
+
+    ResetGoal: function( canvas ) {
+        this.goalX = -1;
+        this.goalY = -1;
     }
 };
 
@@ -66,6 +176,8 @@ gameState = {
     time: 6,
     dropFrequency: 100,
     minObjectY: 110,
+    isNight: false,
+    timeSpeed: 0.01,
 
     people: [],
     items: [],
@@ -78,7 +190,7 @@ gameState = {
         SHOOT:      { code: 32, isDown: false },
     },
 
-    objBear : null,
+    bearPlayer : null,
 
     Init: function( canvas, options ) {
         gameState.canvas = canvas;
@@ -87,15 +199,26 @@ gameState = {
 
         gameState.images.bear = new Image();
         gameState.images.bear.src = "assets/images/bear.png";
+        
+        gameState.images.ranger = new Image();
+        gameState.images.ranger.src = "assets/images/ranger.png";
 
         gameState.images.sky = new Image();
         gameState.images.sky.src = "assets/images/sky.png";
+        
+        gameState.images.cabins = new Image();
+        gameState.images.cabins.src = "assets/images/cabins.png";
 
-        gameState.objBear = bear;
-        gameState.objBear.image = gameState.images.bear;
-        gameState.objBear.x = 640/2 - 48/2;
-        gameState.objBear.y = 480/2 - 48/2;
+        gameState.bearPlayer = bear;
+        gameState.bearPlayer.image = gameState.images.bear;
+        gameState.bearPlayer.x = 640/2 - 48/2;
+        gameState.bearPlayer.y = 480/2 - 48/2;
 
+
+        gameState.ranger = nightRanger;
+        gameState.ranger.image = gameState.images.ranger;
+        gameState.ranger.x = 100;
+        gameState.ranger.y = 100;
         
         gameState.images.people = new Image();
         gameState.images.people.src = "assets/images/people.png";
@@ -139,29 +262,39 @@ gameState = {
     },
 
     Update: function() {
+        if ( gameState.time > 8 && gameState.time < 20 ) {
+            gameState.isNight = false;
+        }
+        else {
+            gameState.isNight = true;
+        }
+        
         if ( gameState.keys.UP.isDown ) {
-            gameState.objBear.Move( "UP" );
+            gameState.bearPlayer.Move( "UP" );
         }
         else if ( gameState.keys.DOWN.isDown ) {
-            gameState.objBear.Move( "DOWN" );
+            gameState.bearPlayer.Move( "DOWN" );
         }
         if ( gameState.keys.LEFT.isDown ) {
-            gameState.objBear.Move( "LEFT" );
+            gameState.bearPlayer.Move( "LEFT" );
         }
         else if ( gameState.keys.RIGHT.isDown ) {
-            gameState.objBear.Move( "RIGHT" );
+            gameState.bearPlayer.Move( "RIGHT" );
         }
 
         // Update bear
-        gameState.objBear.Update();
+        gameState.bearPlayer.Update();
 
         // Update people
         for ( var i = 0; i < gameState.people.length; i++ ) {
             gameState.people[i].Update();
         }
 
+        // Update ranger
+        gameState.ranger.Update();
+
         var drop = Math.floor( Math.random() * gameState.dropFrequency );
-        if ( drop == 0 )
+        if ( drop == 0 && gameState.isNight == false )
         {
             gameState.DropItem();
         }
@@ -169,9 +302,16 @@ gameState = {
         // Check collision
         var removeMe = [];
         for ( var i = 0; i < gameState.items.length; i++ ) {
-            if ( gameState.IsCollision( gameState.objBear, gameState.items[i] )
+            if ( gameState.IsCollision( gameState.bearPlayer, gameState.items[i] ) 
                     && gameState.items[i].type == "food" ) {
-                gameState.objBear.Eat( gameState.items[i].value );
+                gameState.bearPlayer.Eat( gameState.items[i].value );
+                gameState.items[i].eaten = true;
+                removeMe.push( i );
+            }
+
+            if ( gameState.IsCollision( gameState.ranger, gameState.items[i] )
+                    && gameState.items[i].type == "food" ) {
+                gameState.ranger.ResetGoal();
                 gameState.items[i].eaten = true;
                 removeMe.push( i );
             }
@@ -185,7 +325,7 @@ gameState = {
         
 
         // Update timer
-        gameState.time += 0.01;
+        gameState.time += gameState.timeSpeed;
         if ( gameState.time >= 24.0 ) {
             gameState.time = 0.0;
         }
@@ -232,9 +372,12 @@ gameState = {
         for ( var i = 0; i < gameState.people.length; i++ ) {
             gameState.people[i].Draw( main.canvasWindow );
         }
+
+        // Draw ranger
+        gameState.ranger.Draw( main.canvasWindow );
         
         // Draw bear
-        gameState.objBear.Draw( main.canvasWindow );
+        gameState.bearPlayer.Draw( main.canvasWindow );
 
         // Draw UI
         UI_TOOLS.Draw( gameState.canvas );
@@ -296,46 +439,62 @@ gameState = {
                 height: 32,
                 fullWidth: 32,
                 fullHeight: 32,
+                type: Math.floor(Math.random() * 4),
                 speed: 2,
                 isRunning: false,
                 dir: "",
                 countdown: 20,
                 image: gameState.images.people,
                 campfire: null,
+                isAsleep: false,
+                tentType: Math.floor(Math.random() * 4),
 
                 Update: function() {
-                    this.Move( this.dir );
+                    if ( gameState.time > 8 && gameState.time < 20 ) {
+                        this.isAsleep = false;
+                    }
+                    else {
+                        this.isAsleep = true;
+                    }
+                    
+                    // gameState.images.tents
+                    if ( this.isAsleep ) {
+                    }
+                    else {
+                        this.Move( this.dir );
 
-                    // Run from bear
-                    var dist = gameState.GetDistance( this, gameState.objBear );
-                    if ( dist < 100 ) {
-                        this.isRunning = true;
-                        this.dir = "";
-                        
-                        if ( gameState.objBear.y < this.y ) {
-                            this.dir += "S";
+                        // Run from bear
+                        var dist = gameState.GetDistance( this, gameState.bearPlayer );
+                        if ( dist < 100 ) {
+                            this.isRunning = true;
+                            this.dir = "";
+                            
+                            if ( gameState.bearPlayer.y < this.y ) {
+                                this.dir += "S";
+                            }
+                            else if ( gameState.bearPlayer.y > this.y ) {
+                                this.dir += "N";
+                            }
+                            if ( gameState.bearPlayer.x < this.x ) {
+                                this.dir += "E";
+                            }
+                            else if ( gameState.bearPlayer.x > this.x ) {
+                                this.dir += "W";
+                            }
                         }
-                        else if ( gameState.objBear.y > this.y ) {
-                            this.dir += "N";
+                        else
+                        {
+                            this.isRunning = false;
                         }
-                        if ( gameState.objBear.x < this.x ) {
-                            this.dir += "E";
-                        }
-                        else if ( gameState.objBear.x > this.x ) {
-                            this.dir += "W";
-                        }
-                    }
-                    else
-                    {
-                        this.isRunning = false;
-                    }
 
-                    // Or update movement direction
-                    this.countdown -= 1;
-                    if ( this.countdown == 0 ) {
-                        this.ChooseDirection();
-                        this.countdown = Math.floor( Math.random() * 10) + 10;
+                        // Or update movement direction
+                        this.countdown -= 1;
+                        if ( this.countdown == 0 ) {
+                            this.ChooseDirection();
+                            this.countdown = Math.floor( Math.random() * 10) + 10;
+                        }
                     }
+                    
                 },
 
                 ChooseDirection: function() {
@@ -393,11 +552,21 @@ gameState = {
                 },
 
                 Draw: function( canvas ) {
-                    canvas.drawImage(
-                        this.image,
-                        0, 0, this.width, this.height,
-                        this.x, this.y,
-                        this.fullWidth, this.fullHeight );
+                    if ( this.isAsleep ) {
+                        canvas.drawImage(
+                            gameState.images.tents,
+                            this.tentType * this.width, 0, this.width, this.height,
+                            this.x, this.y,
+                            this.fullWidth, this.fullHeight );
+
+                    }
+                    else {
+                        canvas.drawImage(
+                            this.image,
+                            this.type * this.width, 0, this.width, this.height,
+                            this.x, this.y,
+                            this.fullWidth, this.fullHeight );
+                    }
                 }
             };
             person.ChooseDirection();
@@ -411,7 +580,7 @@ gameState = {
         for ( var c = 0; c < 5; c++ )
         {
             var campfire = {
-                x: Math.floor(Math.random() * 640),
+                x: Math.floor(Math.random() * 640-32),
                 y: Math.floor(Math.random() * 380) + gameState.minObjectY,
                 width: 32,
                 height: 32,
@@ -421,9 +590,13 @@ gameState = {
                 type: "campfire",
 
                 Draw: function( canvas ) {
+                    var fireX = 32;
+                    if ( gameState.time > 8 && gameState.time < 20 ) {
+                        fireX = 0;
+                    }
                     canvas.drawImage(
                         this.image,
-                        0, 0, this.width, this.height,
+                        fireX, 0, this.width, this.height,
                         this.x, this.y,
                         this.fullWidth, this.fullHeight );
                 }
@@ -441,6 +614,6 @@ gameState = {
     },
 
     IsCollision: function( obj1, obj2 ) {
-        return ( gameState.GetDistance( obj1, obj2 ) < 10 );
+        return ( gameState.GetDistance( obj1, obj2 ) < 15 );
     }
 };
